4256dc73ad9fbf6f8bfa74593d4b1c6b6ea122ec
Matthias Stangl <matze749@gmail.com>
2019-07-16 20:40:41 +0200

    Initial commit

---
+++ EI-ReadabilityNews.js
@@ -0,0 +1,47 @@
+"use strict";
+
+Module.register("EI-ReadabilityNews",{
+
+	defaults: {
+	},
+
+	article: "",
+
+	start: function() {
+		const self = this;
+
+		// We need to send a notification to the node_helper in order to establish the socket connection
+		// After this the node_helper can send notifications to the module
+		self.sendSocketNotification("START");
+	},
+
+	getDom: function () {
+		const self = this;
+
+		var wrapper = document.createElement("div");
+		wrapper.innerHTML = self.article.content;
+		wrapper.className = "msmall align-left";
+		wrapper.style = "width:1640px; height:850px; word-wrap:break-word;";
+
+		return wrapper;
+	},
+
+	notificationReceived: function(notification, payload, sender) {
+		if (notification === "ARTICLE_INFO_RESPONSE") {
+		  this.sendSocketNotification(notification, payload);
+		}
+	},
+
+	// Is called if we receive a notification from our node_helper.js
+	socketNotificationReceived: function(notification, payload) {
+		const self = this;
+
+		//Log.log(this.name + " received a socket notification: " + notification + " - Payload: " + payload);
+		switch (notification) {
+		case "ARTICLE": // Got new output from the python module about the current hand position.
+			self.article = payload;
+			self.updateDom();
+			break;
+		}
+	},
+});
---
+++ node_helper.js
@@ -0,0 +1,41 @@
+var NodeHelper = require("node_helper");
+var JSDOM = require("jsdom").JSDOM;
+var Readability = require("readability");
+
+module.exports = NodeHelper.create({
+
+	// System is ready to boot
+	start: function() {
+		console.log("EI-ReadabilityNews node_helper started");
+	},
+
+	updateArticle: function(payload) {
+		const self = this;
+
+		// NOTE (MSt): Some websites need javascript to function properly (tagesschau.de currently does not).
+		// If javascript is needed, pass the following option
+		// { runScripts: "dangerously" }
+		JSDOM.fromURL(payload.url).then(dom => {
+			let reader = new Readability(dom.window.document);
+			let article = reader.parse();
+			//console.log(article);
+
+			self.sendSocketNotification("ARTICLE", article);
+		});
+	},
+
+	// System is shutting down
+	stop: function() {
+		console.log("EI-ReadabilityNews node_helper stopped");
+	},
+
+	// My module (EI-ReadabilityNews.js) has sent a notification
+	socketNotificationReceived: function(notification, payload) {
+		const self = this;
+
+		//console.log(this.name + " received a socket notification: " + notification + " - Payload: " + payload);
+		if (notification === "ARTICLE_INFO_RESPONSE") {
+			self.updateArticle(payload);
+		}
+	},
+});
